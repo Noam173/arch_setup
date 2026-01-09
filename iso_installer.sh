20d34e57596151ca046c5070982da10c760fffc6
Noam Cohen <Noamiko173@gmail.com>
2026-01-09 19:56:14 +0200

    changes to iso_installer

---
+++ iso_installer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+out_file="arch.iso"
+url="https://archlinux.interhost.co.il/iso/latest/archlinux-x86_64.iso"
+if [ -f $out_file ]; then
+	rm -rf $out_file
+fi
+if [ -f /usr/bin/wget ]; then
+	wget $url -O $out_file
+else
+	curl $url -o $out_file
+fi
+echo "Date of iso is $(date +"01/%m/%y")"
