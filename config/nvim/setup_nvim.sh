10b9d4dd8e8257159d7f217eaa6f13261df2fd26
Noam Cohen <noamiko173@gmail.com>
2026-01-16 17:31:48 +0200

    idk

--- config/nvim/setup_nvim.sh
+++ config/nvim/setup_nvim.sh
@@ -12,11 +12,3 @@ rm -rf "$HOME/.config/nvim/.git"
 mkdir -p "$HOME/.config/nvim/snippets/"
 file=$(find "$(dirname "$0")" -name "*json" -type f)
 cp "$file" "$HOME/.config/nvim/snippets/"
-
-files=$(find "$(dirname "$0")" -name "*lua" -type f)
-
-mkdir -p "$HOME/.config/nvim/lua/plugins/"
-
-for i in $files; do
-  cp "$i" "$HOME/.config/nvim/lua/plugins"
-done
