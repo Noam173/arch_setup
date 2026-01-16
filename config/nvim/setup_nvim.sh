651ccd7e82c571a39fa395148944bff802d10038
Noam Cohen <noamiko173@gmail.com>
2026-01-16 16:48:34 +0200

    fixed nvim install

--- config/nvim/setup_nvim.sh
+++ config/nvim/setup_nvim.sh
@@ -2,18 +2,16 @@ echo "running nvim setup"
 . "$(dirname "$(dirname "$0")")/install_pkgs.sh"
 idk neovim
 
-if [ ! -d "$HOME/.config/nvim/" ]; then
-  mkdir -p "$HOME/.config/nvim"
+if [ -d "$HOME/.config/nvim/" ]; then
+  rm -rf "$HOME/.config/nvim/"
 fi
+
+git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim/.git"
 
 mkdir -p "$HOME/.config/nvim/snippets/"
 file=$(find "$(dirname "$0")" -name "*json" -type f)
 cp "$file" "$HOME/.config/nvim/snippets/"
-
-if [ ! -d "$HOME/.config/nvim/lua/" ]; then
-  git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
-  rm -rf "$HOME/.config/nvim/.git"
-fi
 
 files=$(find "$(dirname "$0")" -name "*lua" -type f)
 
